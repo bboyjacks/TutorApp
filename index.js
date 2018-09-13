2b70aa59a4fbfed7dea44f1766d375896c9a4d9c
Lester Dela Cruz <lgdelacruz@Lesters-MacBook-Pro-2.local>
2018-09-12 20:28:28 -0700

    Initial Commit

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ function setup() {
     createCanvas (windowWidth, windowHeight);
 }
 
+var x = 0;
+var y = 0;
+var x_prev = 0;
+var y_prev = 0;
+
 function draw() {
-    rect (50, 50, 100, 100);
+}
+
+function mouseDragged() {
+    x = mouseX;
+    y = mouseY;
+    line(x_prev, y_prev, x, y);
+    x_prev = x;
+    y_prev = y;
 }
