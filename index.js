f13588bd9852f5d3db666678a369df3470702892
Lester Dela Cruz <lgdelacruz@Lesters-MacBook-Pro-2.local>
2018-09-12 21:33:29 -0700

    Fixed Draw

--- index.js
+++ index.js
@@ -2,18 +2,9 @@ function setup() {
     createCanvas (windowWidth, windowHeight);
 }
 
-var x = 0;
-var y = 0;
-var x_prev = 0;
-var y_prev = 0;
-
 function draw() {
 }
 
 function mouseDragged() {
-    x = mouseX;
-    y = mouseY;
-    line(x_prev, y_prev, x, y);
-    x_prev = x;
-    y_prev = y;
+    line(mouseX, mouseY, pmouseX, pmouseY);
 }
